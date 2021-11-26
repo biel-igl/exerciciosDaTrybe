0ba003f29efe49589189b1f7f4c54e911ca2953b
biel-igl <gabrielpires524@gmail.com>
2021-11-26 15:46:19 -0300

    primeiro exercicio de fixação do bloco 4

---
+++ bloco 4/4.1/exercico1.js
@@ -0,0 +1,8 @@
+const myName= "Gabriel";
+const birthCity= "Nova Esperança";
+var birthYear=1995;
+birthYear=2030;
+birthCity = "CM";
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
