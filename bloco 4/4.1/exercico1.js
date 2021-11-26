0bf7c3a8723e50b50ad7c7b999154d99920449f9
biel-igl <gabrielpires524@gmail.com>
2021-11-26 19:30:49 -0300

    faz o exercicio 3

--- bloco 4/4.1/exercico1.js
+++ bloco 4/4.1/exercico1.js
@@ -1,12 +1,31 @@
 const a=1;
 const b=2;
+const c=3;
 (a+b);
 (a-b);
 (a*b);
 (a/b);
 (a%b);
+//2 
 if (a>b){
     console.log(a);
 } else{
     console.log(b);
 };
+//3
+const m31=1
+const m32=2
+const m33=3
+if(m31>m32){
+    if(m31>m33){
+        console.log(m31);
+    } else{
+        console.log(m33);
+    }
+} else {
+    if(m32>m33){
+        console.log(m32);
+    } else{
+        console.log(m33)
+    }
+}
