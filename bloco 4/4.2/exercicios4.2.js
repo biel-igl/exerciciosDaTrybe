c7e61028f17ff86c790f689f7b7dc2bb28e8bc50
biel-igl <gabrielpires524@gmail.com>
2021-11-29 20:06:30 -0300

    termina os exercicios propostos

--- bloco 4/4.2/exercicios4.2.js
+++ bloco 4/4.2/exercicios4.2.js
@@ -27,6 +27,49 @@ div=a/numbers.length;
 if (div>20){
     console.log("valor maior que 20");
 } else{
-    console.log("valor menor ou igual a 20")
-}*/
+    console.log("valor menor ou igual a 20");
+}
 //5
+let maior=0;
+for (let a=0;a<numbers.length;a+=1){
+    if (maior<numbers[a]){
+        maior=numbers[a];
+    }
+}
+console.log(maior);
+//6
+let c=0;
+for (let a=0;a<numbers.length;a+=1){
+    if (numbers[a]%2!=0){
+        c+=1;
+    }
+}
+if (c!=0){
+    console.log(c);
+}else {
+    console.log("nenhum valor ímpar encontrado");
+}
+//7
+let menor=(numbers[0]);
+for (let a=0;a<numbers.length;a+=1){
+    if (menor>numbers[a]){
+        menor=numbers[a];
+    }
+}
+console.log(menor);
+//8
+let imp=[];
+for (let a=0;a<25;a+=1){
+    imp.push(a+1);
+}
+console.log(imp);*/
+//9
+let imp=[];
+let div=[];
+for (let a=0;a<25;a+=1){
+    imp.push(a+1);
+}
+for (let c=0;c<imp.length;c+=1){
+    div.push(imp[c]/2);
+}
+console.log(div);
