a2bdb36e8c1936a4736da9012290aadbfc2c697f
biel-igl <gabrielpires524@gmail.com>
2021-11-29 15:49:51 -0300

    faz exercicos de for

---
+++ bloco 4/4.2/exerciciosFor.js
@@ -0,0 +1,4 @@
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+for(let comprar=0;comprar<groceryList.length;comprar +=1){
+    console.log(groceryList[comprar]);
+}
--- bloco 4/4.2/exercicoArrays.js
+++ bloco 4/4.2/exercicoArrays.js
@@ -3,8 +3,13 @@ let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let menuServices = menu[1];
 
 console.log(menuServices); */
-//2
+/* //2
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu.indexOf('Portfólio');
 
-console.log(indexOfPortfolio);
+console.log(indexOfPortfolio); */
+//3
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push("Contato");
+
+console.log(menu);
