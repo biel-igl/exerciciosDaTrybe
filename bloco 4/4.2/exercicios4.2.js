e2960e000bcfaf144f60ea045fee65f6fe92fd39
biel-igl <gabrielpires524@gmail.com>
2021-11-29 18:11:58 -0300

    faz o exercicio 4

---
+++ bloco 4/4.2/exercicios4.2.js
@@ -0,0 +1,32 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* //1
+for (number of numbers){
+    console.log(number);
+}
+//2
+let a=0
+for (soma of numbers){
+    a+=soma
+}
+console.log(a)
+//3
+let a=0;
+let div=0;
+for (soma of numbers){
+    a+=soma;
+}
+div=a/numbers.length;
+console.log(div);
+//4
+let a=0;
+let div=0;
+for (soma of numbers){
+    a+=soma;
+}
+div=a/numbers.length;
+if (div>20){
+    console.log("valor maior que 20");
+} else{
+    console.log("valor menor ou igual a 20")
+}*/
+//5
