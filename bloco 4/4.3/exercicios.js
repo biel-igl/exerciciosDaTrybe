bb3b64b91ba04c0dba54794eb5e9451e9182ba9b
biel-igl <gabrielpires524@gmail.com>
2021-12-08 00:33:03 -0300

    faz exemplos 1 2 e 3

--- bloco 4/4.3/exercicios.js
+++ bloco 4/4.3/exercicios.js
@@ -16,14 +16,16 @@ if (n>=1){
             linha=linha +"*";
         }
         console.log(linha);
-    }*/
+    }
     //3
     for (let a=1;a<=n;a+=1){
         let linha="";
         for (let b=1;b<=n-a;b+=1){
-            linha =linha + "1"
+            linha =linha + "1";
         }
         
         console.log(linha)
-    }
+    }*/
+    //4
+    
 }
